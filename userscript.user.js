7b09b8e653ef7b857c941c36ab530cf4635a9f52
sadnalor <51399106+sadnalor@users.noreply.github.com>
2026-01-14 13:02:11 +0100

    initial

--- userscript.user.js
+++ userscript.user.js
@@ -2,15 +2,15 @@
 // ==UserScript==
 // @name         Service Cloud Premium 3
 // @namespace    https://github.com/sadnalor/tampermonkeys
-// @version      2026.01.14.0
+// @version      2026.01.14.1
 // @author       Roland
 // @description  Internal Salesforce Service Cloud helper
 //
-// @match        https://planview.lightning.force.*/
-// @match        https://planview--trident.lightning.force.*/
-// @match        https://planview--trident.sandbox.lightning.force.*/
-// @match        https://planview--partialsb.lightning.force.*/
-// @match        https://planview--partialsb.sandbox.lightning.force.*/
+// @include      /.*planview.lightning.force.*/
+// @include      /.*planview--trident.lightning.force.*/
+// @include      /.*planview--trident.sandbox.lightning.force.*/
+// @include      /.*planview--partialsb.lightning.force.*/
+// @include      /.*planview--partialsb.sandbox.lightning.force.*/
 //
 // @connect      whoslookingatcases.herokuapp.com
 // @connect      servicecloudpremium-781150c5a240.herokuapp.com
@@ -29,8 +29,6 @@
 // @grant        GM_listValues
 // @grant        GM_setValue
 // @grant        GM_getValue
-//
-// @run-at       document-idle
 //
 // @downloadURL  https://github.com/sadnalor/tampermonkeys/releases/latest/download/userscript.user.js
 // @updateURL    https://github.com/sadnalor/tampermonkeys/releases/latest/download/userscript.user.js
